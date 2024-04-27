a3ff0b6ad2f5a05224f44253ae5976a3b873f055
perfectionist007 <turash.chowdhury@gmail.com>
2024-04-27 21:18:02 +0600

    index.html page with axum
    connected index.html page with axum

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use axum::{
     routing::get,
     Router,
+    response::Html,
 };
+use std::fs;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // Read the contents of the HTML file
+    let html_content = fs::read_to_string("src/assets/template/home/index.html")
+        .expect("Failed to read HTML file");
 
-    // run it with hyper on localhost:3000
+    // Build our application with a single route
+    let app = Router::new().route("/", get(move || {
+        // Return the HTML content as the response
+        async move { Html(html_content.clone()) }
+    }));
+
+    // Run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
